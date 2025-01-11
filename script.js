2bf5d62a36a1c68ef905dcb90919ddee4c301543
SoshuKo <chitian.kokkota@gmail.com>
2025-01-11 14:58:08 +0900

    Update script.js

--- script.js
+++ script.js
@@ -160,7 +160,7 @@ word = word.replace(vowelPattern, (match, p1, p2) => {
         toneChange = 1;
     } else if (/bŭ|dŭ|gŭ|jŭ/.test(consonant)) {
         toneChange = 2;
-    } else if (/gh|ng/.test(consonant)) {
+    } else if (/ghŭ|gh|ng/.test(consonant)) {
         toneChange = 0;
     } else if (/c’|k’|p’|t’|kh|ph|th/.test(consonant)) {
         toneChange = 1;
@@ -171,7 +171,7 @@ word = word.replace(vowelPattern, (match, p1, p2) => {
     const nonVowels = consonant.split('').filter(char => !vowels.includes(char));
 
     // 二文字子音があるかどうかをチェックする
-    if (nonVowels.length === 2 && /gh|kh|ng|ph|sh|th/.test(nonVowels.join(''))) {
+    if (nonVowels.length === 2 && /ghŭ|gh|kh|ng|ph|sh|th/.test(nonVowels.join(''))) {
         consonant = nonVowels.join(''); // 二文字子音をそのまま残す
         shouldApplyToneChange = false; // 子音削除が行われない場合は音調変更しない
     } else if (nonVowels.length > 1) {
